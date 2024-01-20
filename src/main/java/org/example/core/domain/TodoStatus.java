645e4177557938540c568ae4ec967624918b016f
josericardopenase <josericardopenase@gmail.com>
2024-01-21 00:29:57 +0100

    Created class todo status

--- src/main/java/org/example/core/domain/TodoStatus.java
+++ src/main/java/org/example/core/domain/TodoStatus.java
@@ -1,4 +1,13 @@
 package org.example.core.domain;
 
 public class TodoStatus {
+    public boolean isStatus() {
+        return status;
+    }
+
+    public TodoStatus(boolean status) {
+        this.status = status;
+    }
+
+    private final boolean status;
 }
