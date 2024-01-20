9ead62031618edcbeb4e0e2f1398214267bbec5e
josericardopenase <josericardopenase@gmail.com>
2024-01-21 00:29:42 +0100

    Created id class

--- src/main/java/org/example/core/domain/Id.java
+++ src/main/java/org/example/core/domain/Id.java
@@ -1,4 +1,13 @@
 package org.example.core.domain;
 
 public class Id {
+    private final int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public Id(int id) {
+        this.id = id;
+    }
 }
