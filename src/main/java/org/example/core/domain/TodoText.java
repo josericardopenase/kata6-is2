de1d6b90cfd8f4661cdb17bb7f1ae509812eda3a
josericardopenase <josericardopenase@gmail.com>
2024-01-21 00:35:12 +0100

    Created TodoText class

--- src/main/java/org/example/core/domain/TodoText.java
+++ src/main/java/org/example/core/domain/TodoText.java
@@ -1,4 +1,13 @@
 package org.example.core.domain;
 
 public class TodoText {
+    private final String text;
+
+    public String getText() {
+        return text;
+    }
+
+    public TodoText(String text) {
+        this.text = text;
+    }
 }
