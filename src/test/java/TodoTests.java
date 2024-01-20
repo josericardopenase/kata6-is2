0e22bf330ec7453381e18a66432ddac57b7b9de1
josericardopenase <josericardopenase@gmail.com>
2024-01-21 00:25:28 +0100

    Initial commit

---
+++ src/test/java/TodoTests.java
@@ -0,0 +1,11 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class TodoTests {
+    @Test
+    public void write_here_your_test(){
+    }
+
+}
