a389aa72e90bd74820cc73c771d8b72b13c1fddb
josericardopenase <josericardopenase@gmail.com>
2024-01-21 01:01:51 +0100

    Implemented Todo Repository

--- src/main/java/org/example/core/infraestructure/mock/MockTodoRepository.java
+++ src/main/java/org/example/core/infraestructure/mock/MockTodoRepository.java
@@ -3,21 +3,30 @@ package org.example.core.infraestructure.mock;
 import org.example.core.application.ports.TodoRespository;
 import org.example.core.domain.Todo;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class MockTodoRepository implements TodoRespository {
+    private final List<Todo> todos = new ArrayList<>();
     @Override
     public Todo findById(int id) {
-        return null;
+        return todos.stream()
+                .filter(x -> x.getId() == id)
+                .findFirst()
+                .get();
     }
 
     @Override
     public List<Todo> findAll() {
-        return null;
+        return todos
+                .stream()
+                .collect(Collectors.toList());
     }
 
     @Override
     public Todo save(Todo todo) {
-        return null;
+        this.todos.add(todo);
+        return todo;
     }
 }
