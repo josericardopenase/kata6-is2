aeb959a3257da457ed25b139d34791c00c271a22
josericardopenase <josericardopenase@gmail.com>
2024-01-21 01:02:22 +0100

    Implemented MockIdGenerator

--- src/main/java/org/example/core/infraestructure/mock/MockIdGenerator.java
+++ src/main/java/org/example/core/infraestructure/mock/MockIdGenerator.java
@@ -3,8 +3,11 @@ package org.example.core.infraestructure.mock;
 import org.example.core.application.ports.IdGenerator;
 
 public class MockIdGenerator implements IdGenerator {
+
+    static int current = 0;
     @Override
     public int generate() {
-        return 0;
+        current++;
+        return current;
     }
 }
